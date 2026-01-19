54583fc0bc9bd008448e42509b9d2069548c9090
Jorge Morales Pou <jorgemoralespou@gmail.com>
2026-01-19 17:37:21 +0100

    Refactored project_xxx commands

---
+++ client-programs/pkg/utils/browser.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func OpenBrowser(url string) error {
+	var err error
+
+	if url == "" {
+		return fmt.Errorf("url is required")
+	}
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	return err
+}

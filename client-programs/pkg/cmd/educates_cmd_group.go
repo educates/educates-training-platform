7318cd7bf9cd623d77c2da57c801496dcd47aed9
Jorge Morales Pou <jorgemoralespou@gmail.com>
2026-02-13 19:28:34 +0100

    Remove options, which comes from us using kubectl command grouping

--- client-programs/pkg/cmd/educates_cmd_group.go
+++ client-programs/pkg/cmd/educates_cmd_group.go
@@ -1,10 +1,18 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"k8s.io/kubectl/pkg/util/templates"
+)
+
+var (
+	usageOptionsSuffixPattern = regexp.MustCompile(`(?m)^(\s*.+?) \[options\]\s*$`)
+	globalOptionsHintPattern  = regexp.MustCompile(`(?m)^Use ".* options" for a list of global command-line options \(applies to all commands\)\.\n?`)
 )
 
 /*
@@ -82,9 +90,63 @@ func (p *ProjectInfo) NewEducatesCmdGroup() *cobra.Command {
 
 	commandGroups.Add(c)
 
-	templates.ActsAsRootCommand(c, []string{"--help"}, commandGroups...)
-
 	c.AddCommand(p.NewProjectVersionCmd())
+	configureRootHelpTemplates(c, []string{"--help"}, commandGroups...)
 
 	return c
+}
+
+// configureRootHelpTemplates preserves grouped command help output from cobra
+// templates, while removing the synthetic [options] usage suffix and global
+// options hint from displayed usage text.
+func configureRootHelpTemplates(c *cobra.Command, filters []string, groups ...templates.CommandGroup) {
+	templates.ActsAsRootCommand(c, filters, groups...)
+
+	sanitizeCommandUsage(c)
+}
+
+func sanitizeCommandUsage(command *cobra.Command) {
+	originalUsageFunc := command.UsageFunc()
+	originalHelpFunc := command.HelpFunc()
+
+	command.SetUsageFunc(func(cmd *cobra.Command) error {
+		usageBuffer := bytes.NewBuffer(nil)
+
+		originalErr := cmd.ErrOrStderr()
+		cmd.SetErr(usageBuffer)
+		defer cmd.SetErr(originalErr)
+
+		if err := originalUsageFunc(cmd); err != nil {
+			return err
+		}
+
+		_, err := fmt.Fprint(originalErr, sanitizeUsageOutput(usageBuffer.String()))
+		return err
+	})
+
+	command.SetHelpFunc(func(cmd *cobra.Command, args []string) {
+		helpBuffer := bytes.NewBuffer(nil)
+
+		originalOut := cmd.OutOrStdout()
+		cmd.SetOut(helpBuffer)
+		defer cmd.SetOut(originalOut)
+
+		originalHelpFunc(cmd, args)
+		fmt.Fprint(originalOut, sanitizeUsageOutput(helpBuffer.String()))
+	})
+
+	for _, child := range command.Commands() {
+		sanitizeCommandUsage(child)
+	}
+}
+
+func sanitizeUsageOutput(output string) string {
+	cleaned := usageOptionsSuffixPattern.ReplaceAllString(output, "$1")
+	cleaned = globalOptionsHintPattern.ReplaceAllString(cleaned, "")
+
+	for strings.Contains(cleaned, "\n\n\n") {
+		cleaned = strings.ReplaceAll(cleaned, "\n\n\n", "\n\n")
+	}
+
+	return cleaned
 }

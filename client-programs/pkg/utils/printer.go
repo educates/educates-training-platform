864add2d71d8062487f4c4af17062f4f3e21a4c3
Jorge Morales Pou <jorgemoralespou@gmail.com>
2026-02-23 12:17:47 +0100

    Adjust empty strings in PrintTable

--- client-programs/pkg/utils/printer.go
+++ client-programs/pkg/utils/printer.go
@@ -13,12 +13,13 @@ func PrintTable(headers []string, data [][]string) string {
 	// Print headers
 	fmt.Fprintln(w, strings.Join(headers, "\t"))
 
-	// Print data rows
+	// Print data rows (empty string cells are rendered as tab to preserve alignment)
 	for i, row := range data {
-		if i < len(data) - 1 {
-			fmt.Fprintln(w, strings.Join(row, "\t"))
-		}else {
-			fmt.Fprint(w, strings.Join(row, "\t"))
+		parts := replaceEmptyStringsWithTabs(row)
+		if i < len(data)-1 {
+			fmt.Fprintln(w, strings.Join(parts, "\t"))
+		} else {
+			fmt.Fprint(w, strings.Join(parts, "\t"))
 		}
 	}
 
@@ -32,13 +33,25 @@ func PrintKeyValuesTable(data [][]string) string {
 
 	// Print data rows
 	for i, row := range data {
-		if i < len(data) - 1 {
+		if i < len(data)-1 {
 			fmt.Fprintf(w, "%s:\t%s\n", row[0], row[1])
-		}else {
+		} else {
 			fmt.Fprintf(w, "%s:\t%s", row[0], row[1])
 		}
 	}
 
 	w.Flush()
 	return buf.String()
+}
+
+func replaceEmptyStringsWithTabs(data []string) []string {
+	parts := make([]string, len(data))
+	for j, cell := range data {
+		if cell == "" {
+			parts[j] = "\t"
+		} else {
+			parts[j] = cell
+		}
+	}
+	return parts
 }
